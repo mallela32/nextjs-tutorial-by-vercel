c25174919ab742dee196d76d196f5e6c397cb2e5
nitheesh <mnc9505@gmail.com>
2024-02-13 16:22:09 +0530

    fixed font issue

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,5 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lustria } from 'next/font/google';
 import { Open_Sans } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const openSans = Open_Sans({ subsets: ['latin'] });
+export const lusitana = Lustria({
+  weight: '400',
+  subsets: ['latin'],
+});
